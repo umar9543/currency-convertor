a6096310c9402bf6c4669eea4fd7defd2be6c705
umar9543 <115045882+umar9543@users.noreply.github.com>
2023-02-09 03:21:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+ let convertion={
+     "pkr":{
+        "usd":0.0044,
+        "gbp":12,
+        "pkr":1
+
+     },
+          "gbp"  : {
+        "usd":2.34,
+        "gbp":1,
+        "pkr":218
+
+     },
+           "usd": {
+        "usd":1,
+        "gbp":35,
+        "pkr":250
+
+     }}
+     const answers :{
+        from:"pkr"|"gbp"|"usd",
+        to:"pkr"|"gbp"|"usd",
+        amount:number
+     }=await inquirer.prompt([
+            {
+                type:"list",
+                name:"from",
+                choices:["pkr","gbp","usd"],
+                message:"select currency"
+            },
+            {
+                type:"list",
+                name:"to",
+                choices:["pkr","gbp","usd"],
+                message:"select currency"
+            },
+            {
+                type:"number",
+                name:"amount",
+                message:"enter amount"
+            },
+
+     ])
+     const {from,to,amount}=answers
+     if(from && to && amount){
+       let result= convertion[from][to]*amount;
+       console.log(`your convertion from ${from} to ${to} is ${result}`)
+       
+     }else{
+        console.log("invalid input");
+        
+     }
+
+
+
+
+
+ 
